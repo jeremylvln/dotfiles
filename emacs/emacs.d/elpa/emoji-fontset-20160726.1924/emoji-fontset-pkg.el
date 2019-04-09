1e9475acf1fab65e985beda165792cb8f6139f43
Jérémy Levilain <jeremy.levilain@epitech.eu>
2019-04-10 00:50:02 +0200

    refactor: removed useless files

--- emacs/emacs.d/elpa/emoji-fontset-20160726.1924/emoji-fontset-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "emoji-fontset" "20160726.1924" "Set font face for Emoji." 'nil :commit "10be897fa5165fd40fd35a89e38c759e008fa775" :keywords '("emoji" "font" "config") :authors '(("USAMI Kenta" . "tadsan@zonu.me")) :maintainer '("USAMI Kenta" . "tadsan@zonu.me"))
