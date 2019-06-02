e3a711f3d5dc02dd0d5cace777a7a0c3af77f057
Jérémy Levilain <jeremy.levilain@epitech.eu>
2019-06-02 17:44:10 +0200

    feat: add enclosure

--- setup_my_machine.sh
+++ setup_my_machine.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d ~/.config ]; then
+    mkdir ~/.config/
+fi
+
 # BASH
 ln -sf bash/bashrc ~/.bashrc
 
@@ -11,14 +15,17 @@ ln -sf $PWD/emacs/emacs.d ~/.emacs.d
 # FISH
 sudo dnf install fish
 sudo dnf install util-linux-user # For `chsh` command
-mkdir ~/.config/
-mkdir ~/.config/fish/
+
+if [ ! -d ~/.config/fish ]; then
+    mkdir ~/.config/fish/
+fi
+
 ln -sf $PWD/fish/config.fish ~/.config/fish/config.fish
 ln -sf $PWD/fish/functions/ ~/.config/fish/functions
 ln -sf $PWD/fish/completions/ ~/.config/fish/completions
 ln -sf $PWD/fish/fish_variables ~/.config/fish/fish_variables
-chsh -s /bin/fish $USER
+sudo chsh -s /bin/fish $USER
 
 # GIT
 sudo dnf install git
-ln -sf git/gitconfig ~/.gitconfig
+ln -sf $PWD/git/gitconfig ~/.gitconfig
