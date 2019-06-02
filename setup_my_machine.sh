ed50aa35fc2a49046b7d75ba539461187d8f9564
Jérémy Levilain <jeremy.levilain@epitech.eu>
2019-06-02 17:31:39 +0200

    feat: install chsh

--- setup_my_machine.sh
+++ setup_my_machine.sh
@@ -10,9 +10,13 @@ ln -sf emacs/emacs.d ~/.emacs.d
 
 # FISH
 sudo dnf install fish
+sudo dnf install util-linux-user # For `chsh` command
+mkdir ~/.config/
+mkdir ~/.config/fish/
 ln -sf fish/config.fish ~/.config/fish/config.fish
 ln -sf fish/functions/ ~/.config/fish/functions
 ln -sf fish/completions/ ~/.config/fish/completions
+chsh -s /bin/fish $USER
 
 # GIT
 sudo dnf install git
